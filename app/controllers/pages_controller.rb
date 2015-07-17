529c9d7d230bfd47e596d405d8d24f49c55c3d27
akapronik <1pronik@gmail.com>
2015-07-17 17:01:30 +0300

    Ruby 2.2.2

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,23 @@
+class PagesController < ApplicationController
+  
+	def home
+	end
+	
+	def troup_history
+  end
+
+  def galery
+  end
+
+	def video
+	end
+
+  def history
+  end
+
+	def partners
+	end
+	
+  def contacts
+  end
+end
